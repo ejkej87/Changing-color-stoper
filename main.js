4b2044bef2ccba97686f6f6a168e1c874443b46b
ejkej87 <ejkej8@gmail.com7>
2021-04-26 22:31:14 +0200

    add js files

--- main.js
+++ main.js
@@ -1,28 +1,95 @@
-// const baseURL = 'https://api.punkapi.com/v2/beers/'
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const resetBtn = document.querySelector('.reset')
+const historyBtn = document.querySelector('.history')
+const stopwatch = document.querySelector('.stopwatch')
+const time = document.querySelector('.time')
+const timeList = document.querySelector('.time-list')
 
-// const getBeer = id => {
-//   return fetch(`${baseURL}${id}`)
-//     .then(response => response.json())
-//     .then(data => data)
-//     .then(err => err)
-// }
+const infoBtn = document.querySelector('.info')
+const modalShadow = document.querySelector('.modal-shadow')
+const closeModalBtn = document.querySelector('.close')
 
-// using async await
-// const getBeer = async id => {
-//   try {
-//     const response = await fetch(`${baseURL}${id}`)
-//     const data = await response.json()
-//     return data
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
+let timesArr = []
 
-// getBeer(5).then(data => {
-//   const [beer] = data
-//   const { name, description } = beer
+let countTime
+let minutes = 0
+let seconds = 0
 
-//   const beerDescription = `${name} is ${description}.`
+const handleStart = () => {
+  clearInterval(countTime)
 
-//   console.log(beerDescription)
-// })
+  countTime = setInterval(() => {
+    if (seconds < 9) {
+      seconds++
+      stopwatch.textContent = `${minutes}:0${seconds}`
+    } else if (seconds >= 9 && seconds < 59) {
+      seconds++
+      stopwatch.textContent = `${minutes}:${seconds}`
+    } else {
+      minutes++
+      seconds = 0
+      stopwatch.textContent = `${minutes}:00`
+    }
+  }, 1000)
+}
+
+const handleStop = () => {
+  time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`
+  if (stopwatch.textContent !== '0:00') {
+    time.style.visibility = 'visible'
+    timesArr.push(stopwatch.textContent)
+  }
+  clearStuff()
+}
+
+const handlePause = () => {
+  clearInterval(countTime)
+}
+
+const handleReset = () => {
+  time.style.visibility = 'hidden'
+  timesArr = []
+  clearStuff()
+}
+
+const clearStuff = () => {
+  clearInterval(countTime)
+  stopwatch.textContent = `0:00`
+  timeList.textContent = ''
+  minutes = 0
+  seconds = 0
+}
+
+const showHistory = () => {
+  timeList.textContent = ''
+  let num = 1
+  timesArr.forEach(time => {
+    const newTime = document.createElement('li')
+    newTime.innerHTML = `Pomiar nr #${num}: <span>${time}</span>`
+
+    timeList.appendChild(newTime)
+    num++
+  })
+}
+
+const showModal = () => {
+  if (!(modalShadow.style.display === 'block')) {
+    modalShadow.style.display = 'block'
+  } else {
+    modalShadow.style.display = 'none'
+  }
+  modalShadow.classList.toggle('modal-animation')
+}
+
+startBtn.addEventListener('click', handleStart)
+pauseBtn.addEventListener('click', handlePause)
+stopBtn.addEventListener('click', handleStop)
+resetBtn.addEventListener('click', handleReset)
+historyBtn.addEventListener('click', showHistory)
+infoBtn.addEventListener('click', showModal)
+closeModalBtn.addEventListener('click', showModal)
+window.addEventListener('click', e =>
+  e.target === modalShadow ? showModal() : false
+)
